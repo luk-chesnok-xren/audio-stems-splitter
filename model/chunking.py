9edfef4220a7473fa40afdc3370105f79a721331
luk-chesnok-xren <antonfrantsen@gmail.com>
2026-04-29 18:03:43 +0300

    версия с батчингом аудио + метрики

--- model/chunking.py
+++ model/chunking.py
@@ -1,5 +1,6 @@
 import torch
 from torchaudio.transforms import Fade
+import torch.nn.functional as F
 
 def separate_source(
     model,
@@ -7,7 +8,7 @@ def separate_source(
     segment,
     overlap,
     sample_rate,
-    batch_size=16,
+    batch_size=8,
     device=None
 ):  
     chunk_frames = int(sample_rate * (segment + 2*overlap))
@@ -16,34 +17,45 @@ def separate_source(
     
     batch, channels, length = audio.shape
     
-    start = 0
-    end = chunk_frames - overlap_frames
-    
+    def batched_forward_audio():
+        chunks = []
+        positions = []
+        
+        start = 0
+        end = chunk_frames - overlap_frames
+        
+        while end < length:
+            chunk = audio[:, :, start:end]
+            chunks.append(chunk)
+            positions.append(start)
+        
+            if start == 0:
+                start += step
+            else:
+                start += step + overlap_frames
+
+            end = start + chunk_frames
+        
+        for i in range(0, len(chunks), batch_size):
+            batch_list = chunks[i:i+batch_size]
+            batch_list = [F.pad(c, (0, chunk_frames - c.shape[-1])) if c.shape[-1] < chunk_frames else c for c in batch_list]
+            batch = torch.cat(batch_list, dim=0).to(device)
+            with torch.no_grad():
+                batch_out = model.forward(batch)
+                  
+            yield batch_out, positions[i:i+batch_size]
+            
+    result = torch.zeros(batch, len(model.sources), channels, length, device=device)
     fade = Fade(fade_in_len=0, fade_out_len=int(overlap_frames), fade_shape="linear")
     
-    result = torch.zeros(batch, len(model.sources), channels, length, device=device)
-    
-    while end < length:
-        chunk = audio[:, :, start:end]
-        chunk = chunk.to(device)
-        
-        with torch.no_grad():           
-            out = model.forward(chunk)
-        
-        out = fade(out)
-        
-        result[:, :, :, start:end] += out
-
-        if start == 0:
-            fade.fade_in_len = int(overlap_frames)
-            start += step
-        else:  
-            start += step + overlap_frames
+    for batch_out, positions in batched_forward_audio():
+        for i, (chunk, pos) in enumerate(zip(batch_out, positions)):
+            if i != 0: 
+                fade.fade_in_len = overlap_frames
+            elif i == len(positions) - 1:
+                fade.fade_out_len = 0
             
-        end = start + chunk_frames
-        
-        if end >= length-overlap_frames:
-            fade.fade_out_len = 0
+            result[:, :, :, pos:pos+batch_out.shape[-1]] += fade(chunk)
             
     return result
     
