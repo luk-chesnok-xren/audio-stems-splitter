87f56d90b55ea616ce5b9a957925fdb354193e91
luk-chesnok-xren <antonfrantsen@gmail.com>
2026-04-29 20:44:33 +0300

    error handler эндпоинта + комментарии к коду

--- api/routes.py
+++ api/routes.py
@@ -15,26 +15,45 @@ app = Flask(__name__)
 @app.route('/separate', methods=['POST'])
 def separate():
     if 'file' not in request.files:
-        return jsonify({'error': 'no file'}), 400
+        return jsonify({'Ошибка': 'файл отсутствует'}), 400
     
     file = request.files['file']
     
-    audio, sample_rate = librosa.load(io.BytesIO(file.read()), sr=44100, mono=False)
+    if file.filename == '':
+        return jsonify({'Ошибка': 'название файла не должно быть пустым и должно содержать расширение (.wav/mp3)'}), 400
+    
+    allowed = {'mp3', 'wav'}
+    ext = file.filename.rsplit('.', 1)[-1].lower()
+    if ext not in allowed:
+        return jsonify({'Ошибка': f'неподдерживаемый формат файла: {ext}'}), 400
+    
+    try:
+        audio, sample_rate = librosa.load(io.BytesIO(file.read()), sr=None, mono=False)
+        
+        if (audio.shape[-1] / sample_rate > MAX_DURATION) or audio.shape[-1] / sample_rate < MIN_DURATION:
+            return jsonify({'Ошибка': 'аудиофайл не должен иметь длительность менее трёх минут либо более десяти минут.'})
+    except Exception:
+        return jsonify({'error': 'не удалось прочесть загруженный файл'}), 400
+    
+    audio, sample_rate = librosa.load(io.BytesIO(file.read()), sr=None, mono=False)
     
     waveform = torch.tensor(audio).unsqueeze(0)
     
+    #нормализация данных сэмпла
     ref = waveform.mean(0)
     waveform = (waveform - ref.mean()) / ref.std()
     waveform = waveform.to(device)
     
     sources = separate_source(hdemucs, waveform, SEGMENT, OVERLAP, sample_rate, device=device).squeeze(0)
     
+    #денормализация итоговых сэмплов и приведение к исходному масштабу
     ref = ref.to(device)
     sources = sources * ref.std() + ref.mean()
     sources = sources.cpu()
     
     source_names = ["drums", "bass", "other", "vocals"]
     
+    #формирование zip архива 
     zip_buffer = io.BytesIO()
     with zipfile.ZipFile(zip_buffer, 'w') as zf:
         for name, source in zip(source_names, sources):
@@ -43,6 +62,7 @@ def separate():
             zf.writestr(f'{name}.wav', wav_buffer.getvalue())
     
     zip_buffer.seek(0)
+    #ответ веб-сервера
     return send_file(zip_buffer, mimetype='application/zip', download_name='stems.zip')
 
 if __name__ == '__main__':
--- model/chunking.py
+++ model/chunking.py
@@ -11,6 +11,22 @@ def separate_source(
     batch_size=8,
     device=None
 ):  
+    """
+    def separate_source(
+        model, - модель для разделения сэмпла на стемы
+        audio, - данные аудиофайла в формате pytorch тензора
+        segment, - размер сегмента в секундах (чистый сигнал без изменения уровня сигнала)
+        overlap, - длительность линейного изменения уровня сигнала с края сегмента в секундах
+        sample_rate, - частота дискретизации сэмпла
+        batch_size=8,-раазмер батча, прогоняемого через модель
+        device=None - вычислительное устройство
+    )
+    
+    функция реализует батчинг одного трека и его ввод в модель сегментами
+    
+    данная функция возвращает тензор с 4 стемами в формате 
+    [B, sources, channels, length] 
+    """
     chunk_frames = int(sample_rate * (segment + 2*overlap))
     overlap_frames = int(overlap * sample_rate)
     step = int(segment * sample_rate)
@@ -25,10 +41,14 @@ def separate_source(
         end = chunk_frames - overlap_frames
         
         while end < length:
+            #формирование сегментов и сохранение начальных сегментов
             chunk = audio[:, :, start:end]
             chunks.append(chunk)
             positions.append(start)
-        
+
+            #"накладка" конца каждого сегмента на начало следующего сегмента
+            #реализует cross fade на стыках сегментах
+            #так как модель склонна искажать сигнал на краях сегмента 
             if start == 0:
                 start += step
             else:
@@ -36,9 +56,13 @@ def separate_source(
 
             end = start + chunk_frames
         
+        #получение вывода для каждого батча сегментов
         for i in range(0, len(chunks), batch_size):
             batch_list = chunks[i:i+batch_size]
+            
+            #паддинг для последнего сегмента если он меньше стандартного сегмента с учетом накладок
             batch_list = [F.pad(c, (0, chunk_frames - c.shape[-1])) if c.shape[-1] < chunk_frames else c for c in batch_list]
+            
             batch = torch.cat(batch_list, dim=0).to(device)
             with torch.no_grad():
                 batch_out = model.forward(batch)
@@ -48,6 +72,7 @@ def separate_source(
     result = torch.zeros(batch, len(model.sources), channels, length, device=device)
     fade = Fade(fade_in_len=0, fade_out_len=int(overlap_frames), fade_shape="linear")
     
+    #формирование результата 
     for batch_out, positions in batched_forward_audio():
         for i, (chunk, pos) in enumerate(zip(batch_out, positions)):
             if i != 0: 
